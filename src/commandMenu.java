030ec35fe6743cb872a9b92a2becd73115fe2417
= <=>
2017-11-03 10:44:21 -0400

    This is the command Menu with login, I commented out the save since it doesnt work yet

--- src/commandMenu.java
+++ src/commandMenu.java
@@ -1,31 +1,63 @@
-import java.awt.ActionEvent;
 import java.util.Scanner;
-import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.IOException;
 
 public class commandMenu {
-
+	
+		// static string username;
+		
 	public static void main(String[] args) {
+	    String Password = null;
+		String Username = null;
+		
+		Scanner use = new Scanner(System.in);
+		System.out.println("Enter your Username:");
+		String username = use.next();
+		
+		System.out.println("Enter your Password:");
+		String password = use.next();
+		// createFile();
+		
+		
+		System.out.println("Welcome to Pandorum " + username);
+		if(username.equals(Username) && password.equals(Password) )
+		{
+			System.out.println("Welcome to Pandorum");
+		}
+		// else if(username.equals(Username)){
+		//	System.out.println("Incorrect password");
+		//}else if(password.equals(Password))
+		//{System.out.println("Incorrect username");}
+		//else {
+		//System.out.println("Incorrect username and password");}
+		
+		
+		
 		
 	}
-	public static int main1(String[] args)
-	{
-		int scan;
-		Scanner input = new Scanner(System.in);
+
+	// private static void createFile() {
+	// File f = new File("Pandorum" + username + ".txt");
+	// if(!f.exists())
+	// {
+	// 	try
+	// 	{
+	// 		f.createNewFile();
+	// 	}catch (IOexception e)
+	// 	{
+	// 		e.printStackTrace();
+	// 	}
+	// }
+	// public static String getSavePath(){
+	// 	return "" + username + ".txt";
+	// }
 		
-				
-		System.out.println("Hello World");
 		
-		scan = input.nextInt();
-		return scan;
+		
+	
 	}
-	private void saveButton(java.awt.ActionEvent evt) 
-	{
-		JFileChooser sv = new JFileChooser();
+
 		
-		int choice = sv.showOpenDialog(null);
-		
-		
-	}
+
 	
 
-}
