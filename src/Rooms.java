7b1f535bfb6a9ff819bd68cab035e3c9d688baef
=nabilah lasker <nlasker1@ggc.edu>
2017-11-10 10:59:31 -0500

    Updated Rooms Class

--- src/Rooms.java
+++ src/Rooms.java
@@ -11,6 +11,21 @@ public class Rooms {
 	private ItemDrop[] items;
 
 	private Puzzle[] puzzle;
+	
+	Rooms(){
+		name=new String[]{
+				"Staircase Sector","Lounge 1","Intensive Care 3","Operating Viewing Room",
+				"Operating Room","Hallway 2","Lobby 1","Hallway 1",
+				"Intensive Care Overflow 1","Intensive Care Overflow 2","Medical Wing Bathroom","Medical Laboratory",
+				"Bathroom","Bedroom","Office","Planning Room",
+				"Kitchen","Cafeteria","Short Hallway","Lounge",
+				"Bathroom","Showers","North Hall","Recreation Room",
+				"Locker Room","Laboratory","Storage Room","Captain's Hallway",
+				"Command Room","Cryo-Storage","Incubation","Lobby",
+				"DNA Library","Cryo-Storage","Incubation","Lobby",
+				
+		};
+	};
 
 	public String getName(byte id) {
 		return name[id];
