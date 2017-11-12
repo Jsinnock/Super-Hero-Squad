7e99d477a68e49cf539a6cc70160152d32a1ec4c
=nabilah lasker <nlasker1@ggc.edu>
2017-11-12 00:44:10 -0500

    Modified Rooms Class. Added RoomsList.

--- src/Rooms.java
+++ src/Rooms.java
@@ -1,5 +1,73 @@
-public class Rooms {
+import java.util.Observable;
 
+public class Rooms extends Observable{
+	private String nameList;
+	private String descriptionList;
+	private String exitsList;
+	private String monstersList;
+	private String itemsList;
+	private String puzzleList;
+	
+	Rooms(String nameList, String descriptionList, String exitsList, String monstersList,
+			String itemsList, String puzzleList)
+	{
+		this.nameList = nameList;
+		this.descriptionList = descriptionList;
+		this.exitsList = exitsList;
+		this.monstersList = monstersList;
+		this.itemsList = itemsList;
+		this.puzzleList = puzzleList;
+	}
+	
+	public String getNameList() {
+		return nameList;
+	}
+	
+	public void setNameList(String nameList) {
+		this.nameList = nameList;
+	}
+	
+	public String getDescriptionList() {
+		return descriptionList;
+	}
+	
+	public void setDescriptionList(String descriptionList) {
+		this.descriptionList = descriptionList;
+	}
+	
+	public String getExitsList() {
+		return exitsList;
+	}
+	
+	public void setExitsList(String exitsList) {
+		this.exitsList = exitsList;
+	}
+	
+	public String getMonstersList() {
+		return monstersList;
+	}
+	
+	public void setMonstersList(String monstersList) {
+		this.monstersList = monstersList;
+	}
+	
+	public String getItemsList() {
+		return itemsList;
+	}
+	
+	public void setItemsList(String itemsList) {
+		this.itemsList = itemsList;
+	}
+	
+	public String getPuzzleList() {
+		return puzzleList;
+	}
+	
+	public void setPuzzleList(String puzzleList) {
+		this.puzzleList = puzzleList;
+	}
+
+	/**
 	private String[] name;
 
 	private String[] description;
@@ -50,5 +118,6 @@ public class Rooms {
 	public Puzzle getPuzzle(byte id) {
 		return puzzle[id];
 	}
+	**/
 
 }
---
+++ src/RoomsList.java
@@ -0,0 +1,46 @@
+/** Not sure if this is the best way to do it. **/
+public class RoomsList {
+	Rooms Room1 = new Rooms("", "", "", "", "", "");
+	Rooms Room2 = new Rooms("", "", "", "", "", "");
+	Rooms Room3 = new Rooms("", "", "", "", "", "");
+	Rooms Room4 = new Rooms("", "", "", "", "", "");
+	Rooms Room5 = new Rooms("", "", "", "", "", "");
+	Rooms Room6 = new Rooms("", "", "", "", "", "");
+	Rooms Room7 = new Rooms("", "", "", "", "", "");
+	Rooms Room8 = new Rooms("", "", "", "", "", "");
+	Rooms Room9 = new Rooms("", "", "", "", "", "");
+	Rooms Room10 = new Rooms("", "", "", "", "", "");
+	Rooms Room11 = new Rooms("", "", "", "", "", "");
+	Rooms Room12 = new Rooms("", "", "", "", "", "");
+	Rooms Room13 = new Rooms("", "", "", "", "", "");
+	Rooms Room14 = new Rooms("", "", "", "", "", "");
+	Rooms Room15 = new Rooms("", "", "", "", "", "");
+	Rooms Room16 = new Rooms("", "", "", "", "", "");
+	Rooms Room17 = new Rooms("", "", "", "", "", "");
+	Rooms Room18 = new Rooms("", "", "", "", "", "");
+	Rooms Room19 = new Rooms("", "", "", "", "", "");
+	Rooms Room20 = new Rooms("", "", "", "", "", "");
+	Rooms Room21 = new Rooms("", "", "", "", "", "");
+	Rooms Room22 = new Rooms("", "", "", "", "", "");
+	Rooms Room23 = new Rooms("", "", "", "", "", "");
+	Rooms Room24 = new Rooms("", "", "", "", "", "");
+	Rooms Room25 = new Rooms("", "", "", "", "", "");
+	Rooms Room26 = new Rooms("", "", "", "", "", "");
+	Rooms Room27 = new Rooms("", "", "", "", "", "");
+	Rooms Room28 = new Rooms("", "", "", "", "", "");
+	Rooms Room29 = new Rooms("", "", "", "", "", "");
+	Rooms Room30 = new Rooms("", "", "", "", "", "");
+	Rooms Room31 = new Rooms("", "", "", "", "", "");
+	Rooms Room32 = new Rooms("", "", "", "", "", "");
+	Rooms Room33 = new Rooms("", "", "", "", "", "");
+	Rooms Room34 = new Rooms("", "", "", "", "", "");
+	Rooms Room35 = new Rooms("", "", "", "", "", "");
+	Rooms Room36 = new Rooms("", "", "", "", "", "");
+	Rooms Room37 = new Rooms("", "", "", "", "", "");
+	Rooms Room38 = new Rooms("", "", "", "", "", "");
+	Rooms Room39 = new Rooms("", "", "", "", "", "");
+	Rooms Room40 = new Rooms("", "", "", "", "", "");
+	Rooms Room41 = new Rooms("", "", "", "", "", "");
+	Rooms Room42 = new Rooms("", "", "", "", "", "");
+
+}
