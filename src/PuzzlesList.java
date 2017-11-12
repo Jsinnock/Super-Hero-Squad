fbe6f4a558e0e936a40fdb5b14e41f3ecb753836
amercadorodrigue <amercadorodrigue@ggc.edu>
2017-11-11 23:36:49 -0500

    PuzzleList almost dome

--- src/PuzzlesList.java
+++ src/PuzzlesList.java
@@ -3,6 +3,13 @@ public class PuzzlesList {
 
 	AnswerPuzzle puzzle1 = new AnswerPuzzle("A1", "Password: ", "Puzzle 1", "The password must be somewhere in this room", "BeingADoctorRocks73Xx");
 	AnswerPuzzle puzzle3 = new AnswerPuzzle("A11", "Who could have thought that without this, you would become so weak?", "Puzzle 3", "In Earth, we take this for granted. Apple falling on your head?", "Gravity");
-	ItemPuzzle puzzle5 = new ItemPuzzle("C9", "Please Enter the Captain's Key.", "Puzzle 5", "That body in C4 might have something to do with this.", "9A");
+	AnswerPuzzle puzzle4 = new AnswerPuzzle("B6", "Enter Code:", "Puzzle 4", "4 digit code. What year is it again?", "2017");
+	ItemPuzzle puzzle5A = new ItemPuzzle("C9", "Please Enter the Captain's Key.", "Puzzle 5A", "That body in C4 might have something to do with this.", "9A");
+	AnswerPuzzle puzzle5B = new AnswerPuzzle("C9", "Enter Validation Code", "Puzzle 5B", "The key you seek is on a crew member somewhere in this sector. The code can be found somewhere within the recreation room.", "7201");
+	
+	AnswerPuzzle puzzle7 = new AnswerPuzzle("D7", "1734-6732-1476-C327-8977-7643-____-V731-1788-8732-4767-8976-4376", "Puzzle 7", "As you look at the screen you see a long list of incomprehensible numbers and letters that appear to\r\n" + 
+			"be a code of some kind, and you notice a rather hard to ignore gap in the code. Something reminds you of the wall of Hallway D8.", "T732");
+	
+	
 	
 }
