11ba492fb48ccaee7275383eb9c25c4e60b6eedb
=nabilah lasker <nlasker1@ggc.edu>
2017-11-12 20:51:48 -0500

    Edited

--- src/RoomsList.java
+++ src/RoomsList.java
@@ -1,10 +1,10 @@
 /** Not sure if this is the best way to do it. **/
 public class RoomsList {
-	Rooms Room1 = new Rooms("", "", "", "", "", "");
-	Rooms Room2 = new Rooms("", "", "", "", "", "");
-	Rooms Room3 = new Rooms("", "", "", "", "", "");
-	Rooms Room4 = new Rooms("", "", "", "", "", "");
-	Rooms Room5 = new Rooms("", "", "", "", "", "");
+	Rooms Room1 = new Rooms("A1, Staircase Sector", "Using only the flickering lights​ ​from​ ​the​ ​LED’s​ ​in​ ​your​ ​suit,​ ​you​ ​carefully make​ ​your​ ​way​ ​up​ ​the​ ​stairs​ ​holding​ ​onto​ ​the​ ​railing​ ​as​ ​your​ ​boots​ ​grip​ ​the bloody​ ​floor.​ ​At​ ​the​ ​top,​ ​there​ ​are​ ​two​ ​large​ ​automatic​ ​double​ ​doors​ ​to​ ​the east​ ​opening​ ​and​ ​closing​ ​as​ ​if​ ​being​ ​blocked​ ​by​ ​something.", "E1", "", "", "");
+	Rooms Room2 = new Rooms("A2, Lounge 1", "As you enter the dimly​ ​lit​ ​room,​ ​a​ ​feeling​ ​of​ ​normalcy​ ​overcomes​ ​you.​ ​This was​ ​once​ ​a​ ​social​ ​place​ ​full​ ​of​ ​life​ ​as​ ​you​ ​see​ ​a​ ​tv​ ​flickering​ ​and​ ​half​ ​eaten dishes​ ​on​ ​the​ ​table.​ ​Whoever​ ​was​ ​present​ ​left​ ​this​ ​place​ ​with​ ​urgency", "E1, E2", "", "", "");
+	Rooms Room3 = new Rooms("A3, Intensive Care 3", "A vast room full of raggedy​ ​beds​ ​and​ ​worn​ ​blood-stained​ ​medical​ ​rags. Most​ ​of​ ​the​ ​beds​ ​are​ ​occupied​ ​by​ ​what​ ​seems​ ​to​ ​be​ ​empty​ ​vessels​ ​of​ ​the crew.​ ​To​ ​the​ ​west​ ​is​ ​a​ ​supply​ ​closet​ ​that​ ​could​ ​contain​ ​useful​ ​supplies", "E2, E3, E11, E12", "", "", "");
+	Rooms Room4 = new Rooms("A4, Operating Viewing Room", "A very long room​ ​filled​ ​with​ ​mostly​ ​shattered​ ​windows.​ ​Peering​ ​through the​ ​broken​ ​glass,​ ​you​ ​witness​ ​an​ ​automated​ ​operating​ ​machine​ ​on​ ​the​ ​fritz. Perhaps​ ​the​ ​room​ ​contains​ ​details​ ​on​ ​what​ ​happened​ ​to​ ​the​ ​crew", "E3, E4, E5", "", "", "");
+	Rooms Room5 = new Rooms("A5, Operating Room", "Entering the oddly​ ​shaped​ ​room,​ ​the​ ​self​ ​operating​ ​robot​ ​seems​ ​to​ ​be attempting​ ​to​ ​examine​ ​the​ ​remains​ ​of​ ​what​ ​appears​ ​to​ ​be​ ​a​ ​skeleton​ ​with​ ​a perfectly​ ​intact​ ​plastic​ ​cafeteria​ ​tray​ ​still​ ​being​ ​clenched.", "E4", "", "", "");
 	Rooms Room6 = new Rooms("", "", "", "", "", "");
 	Rooms Room7 = new Rooms("", "", "", "", "", "");
 	Rooms Room8 = new Rooms("", "", "", "", "", "");
